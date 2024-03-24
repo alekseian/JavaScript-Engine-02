25815aa7bcecbd1c174280b3c9f4e636e658956e
Aleksei An <aleksei.an@student.cga.school>
2024-03-23 20:29:16 -0400

    solving logic bugs

--- main.js
+++ main.js
@@ -160,3 +160,35 @@ console.timeEnd('Timer1')
 
 
 //practice debugging 
+let crewReady = false;
+let fuelReady = false;
+let fuelLevel = 17000;
+let crewStatus = true;
+let computerStatus = 'green';
+//block1
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   fuelReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   fuelReady = false;
+}
+
+console.log(launchReady);
+
+//block2
+if (crewStatus && computerStatus === 'green'){
+   console.log('Crew & computer cleared.');
+   crewReady = true;
+} else {
+   console.log('WARNING: Crew or computer not ready!');
+   crewReady = false;
+}
+console.log(launchReady);
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
