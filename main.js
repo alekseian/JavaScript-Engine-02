d70e2f23116609beea27c090f3fd9a96378c17d6
Aleksei An <aleksei.an@student.cga.school>
2024-04-10 21:34:23 -0400

    removing

--- main.js
+++ main.js
@@ -126,58 +126,58 @@ function addObject(shape) {
 } 
 
 //test code
-let grade;
-let score = -100; //declare a variable
-switch (true) {
-    case (score < 0): grade = "Invalid Grade"; break;
-    case (score < 60): grade = "F"; break;
-    case (score < 70): grade = "D"; break;
-    case (score < 80): grade = "C"; break;
-    case (score < 90): grade = "B"; break;
-    default: grade = "A";
-}
+// let grade;
+// let score = -100; //declare a variable
+// switch (true) {
+//     case (score < 0): grade = "Invalid Grade"; break;
+//     case (score < 60): grade = "F"; break;
+//     case (score < 70): grade = "D"; break;
+//     case (score < 80): grade = "C"; break;
+//     case (score < 90): grade = "B"; break;
+//     default: grade = "A";
+// }
 
 
 //addition
-const origin = new Vec(100, 100);
+// const origin = new Vec(100, 100);
 
-const vector1 = new Vec(50, 60);
-vector1.renderOrigin = origin;
-vector1.color = "red";
+// const vector1 = new Vec(50, 60);
+// vector1.renderOrigin = origin;
+// vector1.color = "red";
 
-const vector2 = new Vec(-50, 60);
-vector2.renderOrigin = origin;
-vector2.color = "blue";
+// const vector2 = new Vec(-50, 60);
+// vector2.renderOrigin = origin;
+// vector2.color = "blue";
 
-const testVector = vector1.clone().add(vector2);
-testVector.renderOrigin = origin;
+// const testVector = vector1.clone().add(vector2);
+// testVector.renderOrigin = origin;
 
-renderer.renderedAlways.push(vector1, vector2, testVector);
-renderer.texts.push({
-    text: "Vector Addition", 
-    position: origin.clone().addX(-50).addY(-20), 
-    color: "green", 
-    font: "15px serif"
-});
+// renderer.renderedAlways.push(vector1, vector2, testVector);
+// renderer.texts.push({
+//     text: "Vector Addition", 
+//     position: origin.clone().addX(-50).addY(-20), 
+//     color: "green", 
+//     font: "15px serif"
+// });
 
 //subtraction
-const origin2 = new Vec(200, 200);
+// const origin2 = new Vec(200, 200);
 
-const vector3 = new Vec(100, 100);
-vector3.renderOrigin = origin2;
-vector3.color = "red";
+// const vector3 = new Vec(100, 100);
+// vector3.renderOrigin = origin2;
+// vector3.color = "red";
 
-const vector4 = new Vec(80, -20);
-vector4.renderOrigin = origin2;
-vector4.color = "blue";
+// const vector4 = new Vec(80, -20);
+// vector4.renderOrigin = origin2;
+// vector4.color = "blue";
 
-const differenceVector = vector3.clone().subtract(vector4);
-differenceVector.renderOrigin = origin2;
+// const differenceVector = vector3.clone().subtract(vector4);
+// differenceVector.renderOrigin = origin2;
 
-renderer.renderedAlways.push(vector3, vector4, differenceVector);
-renderer.texts.push({
-    text: "Vector Subtraction", 
-    position: origin2.clone().addY(-30), 
-    color: "green", 
-    font: "15px serif"
-});
+// renderer.renderedAlways.push(vector3, vector4, differenceVector);
+// renderer.texts.push({
+//     text: "Vector Subtraction", 
+//     position: origin2.clone().addY(-30), 
+//     color: "green", 
+//     font: "15px serif"
+// });
